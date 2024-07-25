ced60fe3ad5c3e4cc2336b32f9b1bf10e03cd5be
Kaleb Souza <kaleb@sindcomercioto.com>
2024-07-25 13:57:45 -0300

    refactor (Routes-web): Agrupa rotas por middleware de autenticação

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,12 @@ Route::get('/', function () {
 });
 
 // rotas do dashboard
-Route::get('dashboard', [UserController::class, 'index'])->middleware('auth');
-Route::get('users/{user}/edit', [UserController::class, 'edit'])->middleware('auth');
-Route::put('users/{user}', [UserController::class, 'update'])->middleware('auth');
-Route::delete('users/{user}', [UserController::class, 'destroy'])->middleware('auth');
+Route::middleware('auth')->group(function (){Route::get('dashboard', [UserController::class, 'index']);
+Route::get('users/{user}/edit', [UserController::class, 'edit']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+});
+//rota teste
 
 // rotas de login e registro
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
