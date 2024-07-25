9d54e1a844896e37fbdc8c068fc3093c74629691
Kaleb Souza <kaleb@sindcomercioto.com>
2024-07-25 16:30:33 -0300

    refactor - Alteração generica

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,39 @@
 <head>
    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <title>Dashboard</title>
 
 </head>
 <body>
-    <h1>Bem-vindo ao Dashboard!</h1>
+    <h1 class="bg-secondary text-white mb-5 h2 shadow">
+      <button class="btn btn-primary my-1 " type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+  <i class="bi bi-list"></i>
+</button>
+<div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="staticBackdropLabel">MENU </h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      <ul class="nav nav-pills flex-column">
+    
+        <li class="nav-item">
+          <a class="nav-link link-dark" href="#">Tabela a</a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link link-dark" href="#">Tabela b</a>
+        </li> 
+          <li class="nav-item">
+          <a class="nav-link link-dark" href="#">Tabela c</a>
+        </li>
+     
+    </div>
+  </div>
+</div>Dashboard</h1>
    
     @if (session('success'))
         <div>{{ session('success') }}</div>
@@ -34,7 +61,7 @@
                     <td>{{ $user->id }}</td>
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
-                    <?php if ($currentUser->access_level === 1){
+                    <?php if ($user->access_level === 1){
                       echo "<td>Adiministrador<td>";
                     }
                     else {
