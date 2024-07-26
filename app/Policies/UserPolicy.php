530fd8c5535459d6ff484c54643b5f03a4ddb7d9
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-07-26 11:12:42 -0300

    docs formatação do codigo

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -7,7 +7,7 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class UserPolicy
 {
     use HandlesAuthorization;
-    // método update retorna true se o usuário atual é um admin
+    // método update retorna true se o usuário atual é um admin e false caso contrário
     public function update(User $currentUser, User $user)
     {
         return $currentUser->access_level === 1;
