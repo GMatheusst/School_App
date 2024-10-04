5d15d0775f7e58b8020e96d6ce94ce5f1508f7c3
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-10-04 13:50:46 -0300

    Teste

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 // classe User representa um usuário
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
     // propriedades do usuário
     protected $guarded = [];
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Policies;
 
 use App\Models\User;
@@ -7,9 +8,10 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class UserPolicy
 {
     use HandlesAuthorization;
+
     // método update retorna true se o usuário atual é um admin e false caso contrário
     public function update(User $currentUser, User $user)
     {
-        return $currentUser->access_level === 1; 
+        return $currentUser->access_level === 1;
     }
 }
