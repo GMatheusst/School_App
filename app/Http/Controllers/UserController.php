1fb371d8b3f1fbd927ea5b3616bc453e28d8c281
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-10-04 13:47:31 -0300

    :hammer: Docs: formatação do User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,33 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth; 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests; 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 // classe UserController reponsavel por apagar e editar usuários na página de dashboard
 class UserController extends Controller
-{   // metodo index retorna a página de dashboard
-  use AuthorizesRequests; 
+{
+    // metodo index retorna a página de dashboard
+    use AuthorizesRequests;
+
     public function index()
     {
-      $users = User::all(); // Obtém todos os usuários
-      $currentUser = Auth::user(); // Obtém o usuário atual
-    return view('dashboard', compact('users', 'currentUser')); // Retorna a página de dashboard
+        $users = User::all(); // Obtém todos os usuários
+        $currentUser = Auth::user(); // Obtém o usuário atual
+
+        return view('dashboard', compact('users', 'currentUser')); // Retorna a página de dashboard
     }
+
     // metodo edit retorna a página de edição de um usuário
-     public function indexHome()
+    public function indexHome()
     {
-      $users = User::all(); // Obtém todos os usuários
-      $currentUser = Auth::user(); // Obtém o usuário atual
-    return view('home', compact('users', 'currentUser')); // Retorna a página de dashboard
+        $users = User::all(); // Obtém todos os usuários
+        $currentUser = Auth::user(); // Obtém o usuário atual
+
+        return view('home', compact('users', 'currentUser')); // Retorna a página de dashboard
     }
+
     public function edit(User $user)
     {
         $this->authorize('update', $user); // Certifique-se de que apenas admins possam acessar isso
-        return view('edit', compact('user')); // Retorna a página de edição 
+
+        return view('edit', compact('user')); // Retorna a página de edição
     }
 
     // metodo update atualiza os dados de um usuário
@@ -38,7 +44,7 @@ class UserController extends Controller
 
         $request->validate([ // Validação dos dados enviados
             'name' => 'required|string|max:255', // Nome obrigatório e com tamanho máximo de 255 caracteres
-            'email' => 'required|string|email|max:255|unique:users,email,' . $user->id, // Email obrigatório, com tamanho máximo de 255 caracteres e deve ser único
+            'email' => 'required|string|email|max:255|unique:users,email,'.$user->id, // Email obrigatório, com tamanho máximo de 255 caracteres e deve ser único
             'access_level' => 'required|integer|in:0,1', // Nível de acesso obrigatório e deve ser 0 ou 1
         ]);
 
@@ -46,6 +52,7 @@ class UserController extends Controller
         $user->email = $request->email; // Atualiza o email do usuário
         $user->access_level = $request->access_level; // Atualiza o nível de acesso do usuário
         $user->save(); // Salva os dados atualizados
+
         return redirect()->route('dashboard')->with('success', 'Usuário atualizado com sucesso!'); // Redireciona para a página de dashboard com sucesso
     }
 
@@ -53,11 +60,7 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         $user->delete(); // Apaga o usuário
-        return redirect('dashboard')->with('success', 'Usuário deletado com sucesso.');// Redireciona para a página de dashboard com sucesso
+
+        return redirect('dashboard')->with('success', 'Usuário deletado com sucesso.'); // Redireciona para a página de dashboard com sucesso
     }
-    
-    
-
-
-
 }
