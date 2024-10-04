56135a6b31cf783e344ab03f8cc9e7eb15095225
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-10-04 16:35:19 -0300

    Formatação do route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Path;
 use App\Http\Controllers\UserController;
+
 // rotas da aplicação
 
 // rotas de página inicial
@@ -11,21 +12,19 @@ Route::get('/', function () {
 });
 
 // rotas do dashboard
-Route::middleware('auth')->group(function (){ // Método middleware para verificar se o usuário está logado
-Route::resource('users', UserController::class); // Método resource para gerenciar usuários por meio das 4 funções crud
-Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
-Route::get('/home', [UserController::class, 'indexHome'])->name('home'); // Método get para gerenciar usuários e redirecionar para a página de dashboard
+Route::middleware('auth')->group(function () { // Método middleware para verificar se o usuário está logado
+    Route::resource('users', UserController::class); // Método resource para gerenciar usuários por meio das 4 funções crud
+    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+    Route::get('/home', [UserController::class, 'indexHome'])->name('home'); // Método get para gerenciar usuários e redirecionar para a página de dashboard
+    Route::get('alunos', [Path::class, 'indexA'])->name('alunos'); // Método get para exibir a página de alunos
+    Route::get('cursos', [Path::class, 'indexC'])->name('cursos'); // Método get para exibir a página de cursos
+    Route::get('projetos', [Path::class, 'indexP'])->name('projetos'); // Método get para exibir a página de projetos
+    Route::get('contato', [Path::class, 'indexCt'])->name('contato'); // Método get para exibir a página de contato
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout'); // Método post para fazer logout
 });
-
 
 // rotas de login e registro
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login'); // Método get para exibir a página de login
 Route::post('login', [AuthController::class, 'login']); // Método post para fazer login
-Route::post('logout', [AuthController::class, 'logout'])->name('logout'); // Método post para fazer logout
 Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');  // Método get para exibir a página de registro
 Route::post('register', [AuthController::class, 'register']); // Método post para criar um novo usuário
-//Rotas de páginas
-Route::get('alunos', [Path::class, 'indexA'])->name('alunos'); // Método get para exibir a página de alunos
-Route::get('cursos', [Path::class, 'indexC'])->name('cursos'); // Método get para exibir a página de cursos
-Route::get('projetos', [Path::class, 'indexP'])->name('projetos'); // Método get para exibir a página de projetos
-Route::get('contato', [Path::class, 'indexCt'])->name('contato'); // Método get para exibir a página de contato
