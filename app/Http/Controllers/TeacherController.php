6924997aeeea1de010cd104f39d8eec19ce67cad
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-10-05 16:31:19 -0300

    :hammer: "AJeita" os controllers que faltavam

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,63 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreTeacherRequest;
+use App\Http\Requests\UpdateTeacherRequest;
+use App\Models\Teacher;
 
 class TeacherController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function store(StoreTeacherRequest $request)
     {
-        //
+        $teacher = Teacher::create($request->validated());
+
+        return response()->json($teacher, 201);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function update(UpdateTeacherRequest $request, Teacher $teacher)
     {
-        //
-    }
+        $teacher->update($request->validated());
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json($teacher, 200);
     }
 }
--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -11,33 +11,4 @@ class WorksController extends Controller
         
     }
 
-    public function create()
-    {
-        
-    }
-
-    public function store(Request $request)
-    {
-        
-    }
-
-    public function show(string $id)
-    {
-        
-    }
-
-    public function edit(string $id)
-    {
-        
-    }
-
-    public function update(Request $request, string $id)
-    {
-        
-    }
-
-    public function destroy(string $id)
-    {
-        
-    }
 }
