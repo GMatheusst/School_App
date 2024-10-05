69fa9378a87691bc7a8ef69473f4d19ecfc73621
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-10-05 17:34:35 -0300

    :sparkles: Adiciona o controller para Atividades/Trabalhos

--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreWorksRequest;
+use App\Http\Requests\UpdateWorksRequest;
+use App\Models\Work;
 use Illuminate\Http\Request;
 
 class WorksController extends Controller
 {
-    public function index()
-    {
+    public function store(StoreWorksRequest $request){
         
+        $works = Work::create($request->validated());
+        
+        return response()->json($works, 201);
     }
+    
+    public function update(UpdateWorksRequest $request, Work $works){
 
+        $works->update($request->validated());
+
+        return response()->json($works, 200);
+    }
 }
